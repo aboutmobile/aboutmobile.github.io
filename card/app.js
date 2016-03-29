9e34f5b8534813f899ff9112f66ffe24f442aa99
Hyeungshik Jung <hyungshike@gmail.com>
2016-03-29 21:30:56 +0900

    Card news view holding all cards

---
+++ card/app.js
@@ -0,0 +1,60 @@
+var imgs = [
+  {
+    src: 'pics/1.jpg'
+  },
+  {
+    src: 'pics/2.jpg'
+  },
+  {
+    src: 'pics/3.jpg'
+  },
+];
+
+// global index value
+var index = 0;
+var numCards = 15;
+$(document).ready(function() {
+  // insert image to DOM
+  for (var i = 0; i < numCards; i++) {
+    // var $frame = $("#wrapper .card:nth-child(" + (i+1) + ")");
+    // $frame.append('<img class="card-img" src="' + imgs[i].src + '"');
+    var imgString = '<div class="card"><img class="card-img" src="'
+    var imgSrc = 'pics/' + (i+1) + '.png'
+    $("#wrapper").append(imgString + imgSrc + '" /></div>')
+  }
+
+  // get window width
+  var cardWidth = $('#cards').width();
+  $('.card').width(cardWidth);
+  $('#wrapper').width(cardWidth * numCards);
+
+  //FIX
+  $('#cards').height(cardWidth);
+
+  function flipForward() {
+    if (index + 1 >= numCards)
+      return;
+    index++;
+    $('#wrapper').css('left', -cardWidth*index + 'px');
+  }
+
+  function flipBackward() {
+    if (index == 0)
+      return;
+    index--;
+    $('#wrapper').css('left', -cardWidth*index + 'px');
+  }
+
+  $('#cards').click(function() {
+    console.log('clicked');
+    flipForward();
+  })
+
+  var cardsHammer = new Hammer(document.getElementById('cards'));
+  cardsHammer.on('swipeleft', function() {
+    flipForward();
+  })
+  cardsHammer.on('swiperight', function() {
+    flipBackward();
+  })
+})
