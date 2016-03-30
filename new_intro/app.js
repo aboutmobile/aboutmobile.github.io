f2ee4fe5cfe5af47f5635da498cb1ebd729dffd0
Hyeungshik Jung <hyungshike@gmail.com>
2016-03-30 20:48:27 +0900

    horizontal bullet

--- new_intro/app.js
+++ new_intro/app.js
@@ -1,12 +1,49 @@
+// Insert youtube iframe
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// Replace the 'ytplayer' element with an <iframe> and
+// YouTube player after the API code downloads.
+var player;
+function onYouTubePlayerAPIReady() {
+  player = new YT.Player('ytplayer', {
+    height: $(window).innerHeight(),
+    width: $(window).innerWidth(),
+    videoId: 'jNGi9Z2L0cc',
+    events: {
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+// Redirect to next section when video ends
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.ENDED) {
+    window.location.href = "http://www.google.com";
+  }
+}
+
 var index = 0;
 var numCards = 5;
 $(document).ready(function() {
   var cardWidth = $('#cards').width();
+  $('#bullets .bullet:nth-child(' + (index+1) + ')').css('background-color', 'blue');
+
+  function changeBullet(idx) {
+    for (var i = 0; i < 5; i++) {
+      $('#bullets .bullet:nth-child(' + (i+1) + ')').css('background-color', 'red');
+    }
+    $('#bullets .bullet:nth-child(' + (idx+1) + ')').css('background-color', 'blue');
+  }
+
   function flipForward() {
     if (index + 1 >= numCards)
       return;
     index++;
     $('#cards_wrapper').css('transform', 'translateX(-' + ((index)*20) + '%)');
+    changeBullet(index);
   }
 
   function flipBackward() {
@@ -14,6 +51,7 @@ $(document).ready(function() {
       return;
     index--;
     $('#cards_wrapper').css('transform', 'translateX(-' + ((index)*20) + '%)');
+    changeBullet(index);
   }
 
   var cardsHammer = new Hammer(document.getElementById('cards'));
@@ -25,4 +63,11 @@ $(document).ready(function() {
     console.log('swiperight')
     flipBackward();
   });
+
+  //click
+  $("#cards").click(function() {
+    if (index == 4) {
+      playVideo();
+    }
+  })
 })
