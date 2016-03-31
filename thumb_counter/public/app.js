9db84eecedcec988862dff40399e4e677fa17260
Hyeungshik Jung <hyungshike@gmail.com>
2016-03-31 14:46:23 +0900

    Heart chart

--- thumb_counter/public/app.js
+++ thumb_counter/public/app.js
@@ -5,11 +5,48 @@ socket.on('count', function(totalLike) {
 });
 
 socket.on('counters', function(obj) {
-  // $("#count").html(JSON.stringify(obj).replact);
   $("#count").html(JSON.stringify(obj, null, 2).replace(/\n/g, "<br>"))
+  updateData(obj)
 })
 
 $('#reset').click(function() {
   socket.emit('reset');
   return false;
 })
+
+var data = {
+  labels_json: ["chat", "insta", "insta2", "news1" ,"news2"],
+  labels: ["소통의 미학", "나에 대하여", "해쉬태그", "가볍지만", "결코 앝지 않은"],
+  datasets: [
+    {
+        label: "My First dataset",
+        fillColor: "rgba(46,61,81,0.5)",
+        strokeColor: "rgba(46,61,81,0)",
+        highlightFill: "rgba(46,61,81,0.9)",
+        highlightStroke: "rgba(46,61,81,0)",
+        data: [0, 0, 0, 0, 0]
+    }
+  ]
+}
+var ctx = $("#myChart").get(0).getContext("2d");
+var myBarChart = new Chart(ctx).Bar(data);
+
+function updateData(obj) {
+  for (var key in obj) {
+    var keyIndex = data.labels_json.indexOf(key);
+    if (keyIndex != -1)
+      console.log(obj);
+      myBarChart.datasets[0].bars[keyIndex].value = obj[key];
+  }
+  myBarChart.update();
+}
+
+// var dd = {
+//   "insta": 30,
+//   "chat": 7,
+//   "news1": 1,
+//   "news2": 1,
+//   "insta2": 1
+// }
+
+// updateData(dd);
